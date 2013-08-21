ec1fc425075a799ca560142b7938cb6885f0652f
NavaneethBhat <navaneeth.y.v@gmail.com>
2013-08-21 22:20:49 +0530

    check netconnect balance [my first soup!!]

---
+++ checkbalance.py
@@ -0,0 +1,20 @@
+import urllib2
+from BeautifulSoup import *
+request  = urllib2.Request("http://www.rcom.co.in/rcom/Netconnect/Netconnect_Authentication.jsp?MDN=7483099564")
+try:
+	response = urllib2.urlopen(request)
+	soup = BeautifulSoup(response)
+ 	#soup.tag gives the content of that tag:) I want the content from td p which is strong
+	message = soup.td.p.strong
+	# message = message.string # conver to string 
+	message = str (message)
+	print message
+	cut = message.split("RCONNECT Bal is ",1)
+	cutdata = cut[1]
+	balance = cutdata[:cutdata.index(",")]
+	print "Your balance is",balance
+	print "Happy Browsing:)"
+	#print message
+except urllib2.URLError:
+	print "Unable to connect to the site"
+	print "Check your internet connection"
